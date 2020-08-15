cf2fbfcd9250d89e35d736b22770a02116045d69
Steven Cleary <sclearyDev@bitbucket.org>
2020-08-15 17:30:27 -0500

    moved footer to component

---
+++ components/sc-footer/sc-footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="blue-grey lighten-5 pt-6 pb-12">
+    <div class="sc-container mx-auto px-4">
+      <div class="text-h3 pt-8 pb-6">Have a question?</div>
+      <div class="body-1 pb-4">
+        Have a question or want to see code in a private repo? Feel free to
+        email me at the address below.
+      </div>
+      <div
+        class="mt-8 mb-12 py-3 px-6 d-inline-flex align-center rounded primary white--text"
+      >
+        <div class="text-h5 font-weight-light" @click="selectText">
+          my.email@gmail.com
+        </div>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn class="ml-8" text dark v-on="on">
+              <v-icon>mdi-content-copy</v-icon>
+            </v-btn>
+          </template>
+          <span>Copy</span>
+        </v-tooltip>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    selectText(event) {
+      const range = document.createRange()
+      range.selectNode(event.target)
+      window.getSelection().removeAllRanges()
+      window.getSelection().addRange(range)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.sc-container {
+  max-width: 675px;
+  width: 100%;
+}
+</style>
