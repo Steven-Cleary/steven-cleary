20ba62980b63cfe3abaa2bf0f0a418bd1c84bfd7
Steven Cleary <sclearyDev@bitbucket.org>
2020-08-15 16:00:42 -0500

    added technology chips

--- components/financially-financed/financially-financed.vue
+++ components/financially-financed/financially-financed.vue
@@ -16,7 +16,6 @@
       height="auto"
       src="projects\financially-financed\financially-financed.png"
     />
-
     <div class="d-flex mt-8">
       <v-tooltip bottom>
         <template v-slot:activator="{ on }">
@@ -55,6 +54,30 @@
         <span>Images and extra info</span>
       </v-tooltip>
     </div>
+    <v-chip class="my-8" color="yellow darken-2" outlined small>
+      <v-icon small left>mdi-language-javascript</v-icon>
+      JS
+    </v-chip>
+    <v-chip class="my-8 ml-2" color="nuxt" outlined small>
+      <v-icon small left>mdi-nuxt</v-icon>
+      Nuxt
+    </v-chip>
+    <v-chip class="my-8 ml-2" color="vue" outlined small>
+      <v-icon small left>mdi-vuejs</v-icon>
+      Vue
+    </v-chip>
+    <v-chip class="my-8 ml-2" color="vuetify" outlined small>
+      <v-icon small left>mdi-vuetify</v-icon>
+      Vuetify
+    </v-chip>
+    <v-chip class="my-8 ml-2" color="sass" outlined small>
+      <v-icon small left>mdi-sass</v-icon>
+      Sass
+    </v-chip>
+    <v-chip class="my-8 ml-2" color="node" outlined small>
+      <v-icon small left>mdi-nodejs</v-icon>
+      Node
+    </v-chip>
   </div>
 </template>
 
