99d85327891e8024980926194fef559932f854d4
Steven Cleary <sclearyDev@bitbucket.org>
2020-08-15 14:38:52 -0500

    moved financially financed to other project

--- pages/index.vue
+++ pages/index.vue
@@ -8,57 +8,7 @@
       and this site is a place to show off my personal projects.
     </div>
     <v-divider class="my-12" />
-    <div class="overline">Current Project</div>
-    <div class="text-h3 mb-4">Financially Financed</div>
-    <div class="subtitle-1 text--secondary my-4">
-      Jan 2019 - Present
-    </div>
-    <div class="body-1 my-4">
-      This site is a hub for personal finance content and calculators. After
-      getting my first job, I started to have a bunch of questions related to
-      personal finance. I began to create calculators and data visualizations to
-      answer these questions I would have. I thought they were useful so I
-      decided to clean them up and create a site so others could use them.
-    </div>
-
-    <div class="d-flex mt-8">
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on }">
-          <div v-on="on">
-            <v-btn
-              outlined
-              color="primary"
-              @click="() => redirect('https://financiallyfinanced.com/')"
-            >
-              <v-icon left>mdi-launch</v-icon>View
-            </v-btn>
-          </div>
-        </template>
-        <span>View live site</span>
-      </v-tooltip>
-
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on }">
-          <div class="ml-4" v-on="on">
-            <v-btn outlined disabled color="primary">
-              <v-icon left>mdi-github</v-icon>Code
-            </v-btn>
-          </div>
-        </template>
-        <span>Code in private repo</span>
-      </v-tooltip>
-
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on }">
-          <div class="ml-4" v-on="on">
-            <v-btn outlined color="primary">
-              <v-icon left>mdi-information-outline</v-icon>Learn more
-            </v-btn>
-          </div>
-        </template>
-        <span>Images and extra info</span>
-      </v-tooltip>
-    </div>
+    <financially-financed />
     <v-divider class="my-12" />
     <div class="text-h3 my-4">Other projects</div>
     <v-timeline class="my-12" dense>
@@ -149,7 +99,11 @@
 </template>
 
 <script>
+import financiallyFinanced from '~/components/financially-financed/financially-financed.vue'
 export default {
+  components: {
+    financiallyFinanced,
+  },
   methods: {
     redirect(url) {
       window.open(url, '_blank')
