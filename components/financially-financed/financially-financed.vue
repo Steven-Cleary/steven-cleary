7300d7108c230f707bfb68c0bc5743d9b4589171
Steven Cleary <sclearyDev@bitbucket.org>
2020-08-16 12:30:04 -0500

    financially financed formatting added bold text

--- components/financially-financed/financially-financed.vue
+++ components/financially-financed/financially-financed.vue
@@ -6,11 +6,14 @@
       Jan 2019 - Present
     </div>
     <div class="body-1 my-4">
-      This site is a hub for personal finance content and calculators. After
-      getting my first job, I started to have a bunch of questions related to
-      personal finance. I began to create calculators and data visualizations to
-      answer these questions I would have. I thought they were useful so I
-      decided to clean them up and create a site so others could use them.
+      This site is a hub for
+      <span class="text-h6 font-weight-bold"
+        >personal finance content and calculators</span
+      >. After getting my first job, I started to have a bunch of questions
+      related to personal finance. I began to create calculators and data
+      visualizations to answer these questions I would have. I thought they were
+      useful so I decided to clean them up and create a site so others could use
+      them.
     </div>
     <v-img
       class="my-8"
