a9c9406b5ca70ca130547138e96de773cddce38d
Steven Cleary <sclearyDev@bitbucket.org>
2020-08-15 19:46:58 -0500

    added content to financially financed

--- pages/financially-financed.vue
+++ pages/financially-financed.vue
@@ -1,5 +1,54 @@
 <template>
   <div class="sc-container mx-auto px-4">
+    <v-btn class="ma-4 sc-back" fab text to="/">
+      <v-icon dark large>mdi-arrow-left</v-icon>
+    </v-btn>
     <div class="text-h1 my-8">Financially Financed</div>
+    <div class="subtitle-1 text--secondary my-4">
+      Jan 2019 - Present
+    </div>
+    <div class="body-1 my-4">
+      This site is a hub for
+      <span class="text-h6 font-weight-bold"
+        >personal finance content and calculators</span
+      >. After getting my first job, I started to have a bunch of questions
+      related to personal finance. I began to create calculators and data
+      visualizations to answer these questions I would have. I thought they were
+      useful so I decided to clean them up and create a site so others could use
+      them.
+    </div>
+    <v-img
+      class="my-8"
+      height="auto"
+      src="projects\financially-financed\financially-financed.png"
+    />
+    <div class="body-1 my-4">
+      This site is a hub for
+      <span class="text-h6 font-weight-bold"
+        >personal finance content and calculators</span
+      >. After getting my first job, I started to have a bunch of questions
+      related to personal finance. I began to create calculators and data
+      visualizations to answer these questions I would have. I thought they were
+      useful so I decided to clean them up and create a site so others could use
+      them.
+    </div>
+    <div class="body-1 my-4">
+      This site is a hub for
+      <span class="text-h6 font-weight-bold"
+        >personal finance content and calculators</span
+      >. After getting my first job, I started to have a bunch of questions
+      related to personal finance. I began to create calculators and data
+      visualizations to answer these questions I would have. I thought they were
+      useful so I decided to clean them up and create a site so others could use
+      them.
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.sc-back {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
