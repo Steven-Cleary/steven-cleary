2fd1d643c855dbb39c574ce8bc45c2d8d9ef0bc3
Steven Cleary <sclearyDev@bitbucket.org>
2020-08-16 18:08:06 -0500

    added content to survey says

--- pages/survey-says.vue
+++ pages/survey-says.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="text-h1 my-8">Survey Says</div>
+    <div class="text-h1 mt-12 mb-4">Survey Says</div>
     <div class="subtitle-1 text--secondary my-4">
       Sept 2017 - April 2018
     </div>
@@ -10,14 +10,77 @@
       tweets.
     </div>
     <v-img
-      class="my-8"
+      class="mt-8"
       height="auto"
       src="projects\Curiocity\survey-says.svg"
     />
+    <div class="text-caption mt-2">
+      This shows a users flow of seeing a question, clicking on the questions to
+      see the possible answers, and after answering the question, seeing the
+      results.
+    </div>
+
+    <div class="text-h5 mt-12">Backend</div>
+    <div class="text-subtitle-2 mt-8">Tests</div>
+    <div class="body-1 my-2">
+      The backend contains tests for the various parts of the api. The backend
+      also contains commands to run all of the tests or just specific test
+      files. Each part of the api has its own test file.
+    </div>
+    <div class="text-subtitle-2 mt-8">Security</div>
+    <div class="body-1 my-2">
+      This app lets users create surveys and answer surveys that other users
+      have created. The surveys are ranked by a model that was trained on
+      tweets.
+    </div>
+    <div class="text-subtitle-2 mt-8">Data Collection</div>
+    <div class="body-1 my-2">
+      The main inspiration behind this app is that it can be used to collect
+      really useful data sets. Every time the user answers a question their
+      response is saved to the database. This means you could go through every
+      user and retrieve all of their answers.
+    </div>
+    <!-- height="auto"
+            max-width="251" -->
+    <div class="text-h5 mt-12">Gallery</div>
+    <div class="d-flex flex-wrap justify-start">
+      <v-img
+        v-for="img in images"
+        :key="img"
+        class="mt-12 mr-12 elevation-6 pointer"
+        height="auto"
+        max-width="251"
+        :src="`projects\\Curiocity\\4-30-18\\${img}`"
+      />
+    </div>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      images: [
+        'AccountOptions.PNG',
+        'CreateNewQuestion.PNG',
+        'Example.PNG',
+        'History.PNG',
+        'Home.PNG',
+        'HomeQuestionOptions.PNG',
+        'HomeQuestionResults.PNG',
+        'HomeScrollDown.PNG',
+        'HomeScrollDownThenUp.PNG',
+        'NewQuestion1.PNG',
+        'NewQuestion2.PNG',
+        'NewQuestionInFeed.PNG',
+        'SearchQuestion1.PNG',
+        'SearchQuestion2.PNG',
+        'Signin.PNG',
+        'Signup.PNG',
+        'Start.PNG',
+        'Vis2.PNG',
+      ],
+    }
+  },
   layout: 'project',
 }
 </script>
