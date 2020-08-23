e574f43ad017bb2f6c3d70ddfb420b4e7fff5783
Steven Cleary <sclearyDev@bitbucket.org>
2020-08-23 15:44:57 -0500

    financially financed content

--- pages/financially-financed.vue
+++ pages/financially-financed.vue
@@ -5,39 +5,64 @@
       Jan 2019 - Present
     </div>
     <div class="body-1 my-4">
-      This site is a hub for
-      <span class="text-h6 font-weight-bold"
-        >personal finance content and calculators</span
-      >. After getting my first job, I started to have a bunch of questions
-      related to personal finance. I began to create calculators and data
-      visualizations to answer these questions I would have. I thought they were
-      useful so I decided to clean them up and create a site so others could use
-      them.
-    </div>
-    <v-img
-      class="my-8 rounded"
-      height="auto"
-      src="projects\financially-financed\financially-financed.png"
-    />
-    <div class="body-1 my-4">
-      This site is a hub for
-      <span class="text-h6 font-weight-bold"
-        >personal finance content and calculators</span
-      >. After getting my first job, I started to have a bunch of questions
-      related to personal finance. I began to create calculators and data
-      visualizations to answer these questions I would have. I thought they were
-      useful so I decided to clean them up and create a site so others could use
-      them.
+      I enjoy personal finance. Specifically I like to create different
+      scenarios and see how things play out. For example how much money would I
+      have if I invested $500 a month for 30 years.
     </div>
     <div class="body-1 my-4">
-      This site is a hub for
-      <span class="text-h6 font-weight-bold"
-        >personal finance content and calculators</span
-      >. After getting my first job, I started to have a bunch of questions
-      related to personal finance. I began to create calculators and data
-      visualizations to answer these questions I would have. I thought they were
-      useful so I decided to clean them up and create a site so others could use
-      them.
+      And I would often use JavaScript to play out these different scenarios.
+      Overtime I started building out interfaces and data visualizations as
+      well.
+    </div>
+    <div class="body-1 my-4">
+      Now at this time I was working two other projects. But I decided that
+      these projects were overly ambitious. And I was looking to create a new
+      project that wasn't overly ambitious. Something that I could get done, and
+      that people would use.
+    </div>
+    <div class="body-1 my-4">
+      So I decided that I want to create a site where I can post calculators and
+      content related to personal finance. But if you are reading this you are
+      probably more concerned with the tech that this site was built with.
+    </div>
+
+    <div class="text-h3 my-4">Benefits</div>
+    <div class="blue-grey lighten-5 pa-4 rounded">
+      <div class="text-h5 font-weight-light my-4">Custom Component Library</div>
+      <div class="body-1 my-4">
+        We built ourselves a rich set of custom components. This makes writing
+        our calculators easy.
+      </div>
+      <v-divider class="my-10" />
+      <div class="text-h5 font-weight-light my-4">Easy Deployments</div>
+      <div class="body-1 my-4">
+        If I want to deploy, I just run a couple of commands and my code is
+        deployed.
+      </div>
+      <v-divider class="my-10" />
+      <div class="text-h5 font-weight-light my-4">SEO</div>
+      <div class="body-1 my-4">
+        We built our site with NuxtJS, which gives us the ability to SSR(Server
+        Side Render) or CSR(Client Side Render) content. This allows us to
+        create content that search engines can read but also client side render
+        more complex content to improve page load speed and user experience.
+      </div>
+      <v-divider class="my-10" />
+      <div class="text-h5 font-weight-light my-4">Material UI</div>
+      <div class="body-1 my-4">
+        We use VuetifyJS.
+        <span class="font-italic"
+          >"Vuetify is a Vue UI Library with beautifully handcrafted Material
+          Components"</span
+        >
+        - Vuetify Landing Page. This allows us to create great user experiences
+        with very little effort.
+      </div>
+    </div>
+    <div class="body-1 my-4 mt-8">
+      If you have read this far you may want to see the code. So send me an
+      email. In the footer below I have attached an email where you can reach
+      me.
     </div>
   </div>
 </template>
