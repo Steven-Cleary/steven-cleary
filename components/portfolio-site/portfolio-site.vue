5290e7d35e547616f31e722c098066c704baafb8
Steven Cleary <sclearyDev@bitbucket.org>
2020-09-10 19:13:46 -0500

    added a component for the technology chips

--- components/portfolio-site/portfolio-site.vue
+++ components/portfolio-site/portfolio-site.vue
@@ -51,39 +51,18 @@
       </v-tooltip>
     </div>
     <div class="text-h6 my-4 mb-2 mt-12">Technologies</div>
-    <v-chip color="yellow darken-2" outlined small>
-      <v-icon small left>mdi-language-javascript</v-icon>
-      JS
-    </v-chip>
-    <v-chip class="ml-2" color="nuxt" outlined small>
-      <v-icon small left>mdi-nuxt</v-icon>
-      Nuxt
-    </v-chip>
-    <v-chip class="ml-2" color="vue" outlined small>
-      <v-icon small left>mdi-vuejs</v-icon>
-      Vue
-    </v-chip>
-    <v-chip class="ml-2" color="vuetify" outlined small>
-      <v-icon small left>mdi-vuetify</v-icon>
-      Vuetify
-    </v-chip>
-    <v-chip class="ml-2" color="docker" outlined small>
-      <v-icon small left>mdi-docker</v-icon>
-      Docker
-    </v-chip>
-    <v-chip class="ml-2" color="sass" outlined small>
-      <v-icon small left>mdi-sass</v-icon>
-      Sass
-    </v-chip>
-    <v-chip class="ml-2" color="node" outlined small>
-      <v-icon small left>mdi-nodejs</v-icon>
-      Node
-    </v-chip>
+    <technology-chips
+      :technologies="['js', 'nuxt', 'vue', 'vuetify', 'docker', 'sass', 'node']"
+    />
   </div>
 </template>
 
 <script>
+import technologyChips from '~/components/technology-chips/technology-chips.vue'
 export default {
+  components: {
+    technologyChips,
+  },
   methods: {
     redirect(url) {
       window.open(url, '_blank')
---
+++ components/technology-chips/technology-chips.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <v-chip
+      v-if="technologies.includes('js')"
+      color="yellow darken-2"
+      outlined
+      small
+    >
+      <v-icon small left>mdi-language-javascript</v-icon>
+      JS
+    </v-chip>
+    <v-chip
+      v-if="technologies.includes('nuxt')"
+      class="ml-2"
+      color="nuxt"
+      outlined
+      small
+    >
+      <v-icon small left>mdi-nuxt</v-icon>
+      Nuxt
+    </v-chip>
+    <v-chip
+      v-if="technologies.includes('vue')"
+      class="ml-2"
+      color="vue"
+      outlined
+      small
+    >
+      <v-icon small left>mdi-vuejs</v-icon>
+      Vue
+    </v-chip>
+    <v-chip
+      v-if="technologies.includes('vuetify')"
+      class="ml-2"
+      color="vuetify"
+      outlined
+      small
+    >
+      <v-icon small left>mdi-vuetify</v-icon>
+      Vuetify
+    </v-chip>
+    <v-chip
+      v-if="technologies.includes('docker')"
+      class="ml-2"
+      color="docker"
+      outlined
+      small
+    >
+      <v-icon small left>mdi-docker</v-icon>
+      Docker
+    </v-chip>
+    <v-chip
+      v-if="technologies.includes('sass')"
+      class="ml-2"
+      color="sass"
+      outlined
+      small
+    >
+      <v-icon small left>mdi-sass</v-icon>
+      Sass
+    </v-chip>
+    <v-chip
+      v-if="technologies.includes('node')"
+      class="ml-2"
+      color="node"
+      outlined
+      small
+    >
+      <v-icon small left>mdi-nodejs</v-icon>
+      Node
+    </v-chip>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    technologies: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
+
+<style lang="scss"></style>
